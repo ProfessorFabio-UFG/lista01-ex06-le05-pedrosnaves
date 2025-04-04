8f4b518c0e3f8d8b329a26ca777d59e5bc06fbc7
pedrosnaves <pedrosnaves@gmail.com>
2025-04-04 18:42:45 -0300

    Update Funcionario.java

--- ex06/Funcionario.java
+++ ex06/Funcionario.java
@@ -1,63 +1,28 @@
-
-
-public class Funcionario extends Pessoa {
+public class Funcionario {
+    private String nome;
     private int matricula;
+    private String telefone;
     private double salario;
-    private String data_ingresso;
-    private String cargo;
+    private String sexo;
 
-    public Funcionario(String nome, String telefone, String cpf, String data_nascimento, String sexo, int matricula, double salario, String data_ingresso, String cargo) {
-       super(nome, telefone, cpf, data_nascimento, sexo);
-       this.matricula = matricula;
-       this.salario = salario;
-       this.data_ingresso = data_ingresso;
-       this.cargo = cargo;
+    public Funcionario(String nome, int matricula){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.telefone = "";
+        this.salario = 0;
+        this.sexo = "";
     }
 
-    public Funcionario(Pessoa p, int matricula, double salario, String data_ingresso, String cargo){
-        super(p.getNome(), p.getTelefone(), p.getCpf(), p.getData_nascimento(), p.getSexo());
-        this.matricula = matricula;
-        this.salario = salario;
-        this.data_ingresso = data_ingresso;
-        this.cargo = cargo;
+    public String getNome() {
+        return this.nome;
     }
 
     public int getMatricula() {
         return this.matricula;
     }
 
-    public double getSalario() {
-        return this.salario;
-    }
 
-    public String getData_ingresso() {
-        return this.data_ingresso;
-    }
 
-    public String getCargo() {
-        return this.cargo;
-    }
-
-    public double aumentoSalario(double percentual){
-        this.salario = this.salario + (this.salario * (percentual/100));
-        return this.salario;
-    }
-
-    public double getRecebimentoAnual(){
-            return this.salario *12;
-    }
-
-    public String toString(){
-        return "\nNome: " + getNome() +
-                "\nTelefone: " + getTelefone() +
-                "\nCPF: " + getCpf() +
-                "\nData de nascimento: " + getData_nascimento() +
-                "\nSexo: " + getSexo() +
-                "\nMatricula: " + this.matricula +
-                "\nSalario: " + this.salario +
-                "\nData de ingresso: " + this.data_ingresso +
-                "\nSexo: " + getSexo() +
-                "\nCargo: " + this.cargo +
-                "\n--------------------\n";
-    }
 }
+
+
