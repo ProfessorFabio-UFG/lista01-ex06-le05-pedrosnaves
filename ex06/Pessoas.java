79e6907e730ad60a2f99bf26eaa905e587d507cd
pedrosnaves <pedrosnaves@gmail.com>
2025-04-16 22:22:14 -0300

    Update and rename Pessoa.java to Pessoas.java

---
+++ ex06/Pessoas.java
@@ -0,0 +1,18 @@
+public class Pessoas {
+    private String nome;
+    private String telefone;
+   
+
+    public Pessoas(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
