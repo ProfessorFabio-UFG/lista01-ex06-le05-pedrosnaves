9100e47b7334aa1fcf1b9cb5ac8b78ec24706c1b
pedrosnaves <pedrosnaves@gmail.com>
2025-04-16 22:24:50 -0300

    Update Cadastro.java

--- ex06/Cadastro.java
+++ ex06/Cadastro.java
@@ -1,35 +1,38 @@
-import java.util.Scanner;
-
 public class Cadastro {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
 
-        Funcionario f1 = new Funcionario("Claudio", 1111);
-        Funcionario f2 = new Funcionario("Beatriz", 2222);
+        Pessoas p1 = new Pessoas("Claudio","62999541235");
+        Pessoas p2 = new Pessoas("Beatriz","62999541235");
+        Pessoas p3 = new Pessoas("Paulo", "62988412587");
+        Pessoas p4 = new Pessoas("Ana Julia", "62991425656");
+        Pessoas p5 = new Pessoas("Joyce", "62999225644");
 
-        Caixa c1 = new Caixa(f1, "62999541235", 2500.00, 7);
+        Funcionarios f1 = new Funcionarios(p1, 1111);
+        Funcionarios f2 = new Funcionarios(p2, 2222);
 
-        Gerente g1 = new Gerente (f2, "62999541235", 10500.00, 500.00, "pf");
+        Gerente g = new Gerente(f1, 10500.00, 500.00, "pf");
+        Caixa c = new Caixa(f2, 2500.00, 7);
 
-        Cliente cliente1 = new Cliente ("Paulo", "62988412587", "74111565488", 25);
-        Cliente cliente2 = new Cliente ("Ana Julia", "62991425656", "55680146520", 69);
-        Cliente cliente3 = new Cliente ("Joyce", "62999225644", "112506258400", 71);
+        Cliente c1 = new Cliente(p3, 25, "74111565488");
+        Cliente c2 = new Cliente(p4, 69, "55680146520");
+        Cliente c3 = new Cliente(p5, 71, "112506258400");
 
 
-        c1.AumentoSalario(10);
+        c.AumentoSalario(10);
+        System.out.println(c);
+        System.out.println("Salario anual:");
+        System.out.println(c.RecebimentoAnual());
+
+        g.AumentoSalario(20);
+        System.out.println(g);
+        System.out.println("Salario anual:");
+        System.out.println(g.RecebimentoAnual());
+
+        c2.desativar();
+        System.out.println(c2);
         System.out.println(c1);
-        System.out.println("Salario anual:");
-        System.out.println(c1.RecebimentoAnual());
-
-        g1.AumentoSalario(20);
-        System.out.println(g1);
-        System.out.println("Salario anual:");
-        System.out.println(g1.RecebimentoAnual());
-
-        cliente2.desativar();
-        System.out.println(cliente2);
-        System.out.println(cliente1);
-        System.out.println(cliente3);
+        System.out.println(c3);
 
     }
 }
